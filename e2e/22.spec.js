1a4fe6ef314b2821a5a9586dc0cf3f553e6c9316
Jesper Oja <jesper.oja@metropolia.fi>
2022-11-14 21:51:09 +0200

    Tehtävät muutettu käyttämään Testingbottia

--- e2e/22.spec.js
+++ e2e/22.spec.js
@@ -1,5 +1,31 @@
 import { test, expect } from '@playwright/test';
+const dotenv = require('dotenv').config();
+
+const SECRET = process.env.SECRET;
+const API_KEY = process.env.API_KEY;
+
+const test = base.test.extend({
+  browser: async ({
+    playwright,
+    browserName,
+    headless,
+    channel,
+    launchOptions
+  }, use) => {
+    const browser = await playwright.chromium.connectOverCDP({
+      wsEndpoint: `wss://cloud.testingbot.com?key=${API_KEY}&secret=${SECRET}&browserName=chrome&browserVersion=latest`,
+    });
+    await use(browser)
+    await browser.close()
+  },
+})
+
 test('test', async ({ page }) => {
+
+  await page.goto('https://testingbot.com')
+  const title = await page.title()
+  base.expect(title).toMatch('TestingBot')
+
   await page.goto('https://areena.yle.fi/tv/opas');
   const uutiset = await page.$$('span:has-text("22.00 Kymmenen uutiset")');
 });
--- e2e/emailwrongformat.spec.js
+++ e2e/emailwrongformat.spec.js
@@ -1,7 +1,34 @@
 import { test, expect } from '@playwright/test';
+const dotenv = require('dotenv').config();
+
+const SECRET = process.env.SECRET;
+const API_KEY = process.env.API_KEY;
+
+const test = base.test.extend({
+  browser: async ({
+    playwright,
+    browserName,
+    headless,
+    channel,
+    launchOptions
+  }, use) => {
+    const browser = await playwright.chromium.connectOverCDP({
+      wsEndpoint: `wss://cloud.testingbot.com?key=${API_KEY}&secret=${SECRET}&browserName=chrome&browserVersion=latest`,
+    });
+    await use(browser)
+    await browser.close()
+  },
+})
+
 
 test('Yle Areena Sign Up Email wrong format test'
 , async ({ page }) => {
+
+  
+  await page.goto('https://testingbot.com')
+  const title = await page.title()
+  expect(title).toMatch('TestingBot')
+
   await page.goto('https://areena.yle.fi/tv');
   await expect(page).toHaveTitle('Yle Areena – Enemmän kuin ehdit katsoa ja kuunnella | TV | Areena | yle.fi')
 
