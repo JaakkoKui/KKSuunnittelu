7e3f0576e7e338a33b5a85cf788eec13d1212e59
Jesper Oja <jesper.oja@metropolia.fi>
2022-11-14 21:47:19 +0200

    Jaakon versio muutettu käyttämään .env

--- e2e/nameandlogo.spec.js
+++ e2e/nameandlogo.spec.js
@@ -1,14 +1,41 @@
-import { test, expect } from '@playwright/test';
+const base = require('@playwright/test')
+const dotenv = require('dotenv').config();
+
+const SECRET = process.env.SECRET;
+const API_KEY = process.env.API_KEY;
+
+const test = base.test.extend({
+  browser: async ({
+    playwright,
+    browserName,
+    headless,
+    channel,
+    launchOptions
+  }, use) => {
+    const browser = await playwright.chromium.connectOverCDP({
+      wsEndpoint: `wss://cloud.testingbot.com?key=${API_KEY}&secret=${SECRET}&browserName=chrome&browserVersion=latest`,
+    });
+    await use(browser)
+    await browser.close()
+  },
+})
+
+
 test('Test logos and names', async ({ page }) => {
+
+  await page.goto('https://testingbot.com')
+  const title = await page.title()
+  base.expect(title).toMatch('TestingBot')
+
   await page.goto('https://areena.yle.fi/tv/opas');
 
   const TV1 = page.locator('[aria-label="Yle TV1"]')
-  await expect(TV1).toHaveClass('channel-header__logo ')
+  await base.expect(TV1).toHaveClass('channel-header__logo ')
 
   const TV2 = page.locator('[aria-label="Yle TV2"]')
-  await expect(TV2).toHaveClass('channel-header__logo ')
+  await base.expect(TV2).toHaveClass('channel-header__logo ')
 
   const HERO = page.locator('[aria-label="Hero"]')
-  await expect(HERO).toHaveClass('channel-header__logo ')
+  await base.expect(HERO).toHaveClass('channel-header__logo ')
 
 })
